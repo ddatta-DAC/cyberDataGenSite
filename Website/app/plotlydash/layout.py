3c35f64ceaaa622d936e61741b3b6e33dd71b435
ddatta <ddatta@vt.edu>
2020-07-02 10:57:25 -0400

    adding updates

--- Website/app/plotlydash/layout.py
+++ Website/app/plotlydash/layout.py
@@ -10,28 +10,29 @@ html_layout = '''
             <link rel="shortcut icon" href="{{ url_for('static', filename='favicon.ico') }}">
             {%css%}
         </head>
-        
+     
     <body id="dash_body">   
         <nav class="navbar fixed-top navbar-light bg-light">
-        <div class="container d-flex align-content-around  flex-wrap">
-            <div class="d-flex  justify-content-start ">
-                <div class=" align-self-start h2" >
-                   <a class="nav-item nav-link" href="/home">Home</a>
+        <div class="flex-grow-1 d-flex">
+            <div class="d-flex">
+                <div class="d-flex " >
+                   <a class="nav-item nav-link h3" href="/home">Home</a>
                 </div>
-                <div  class="align-self-end h2">
-                    <a class="nav-item nav-link active" href="/dashapp">Visualization Dashboard</a>
+                <div  class="d-flex justify-content-end">
+                    <a class="nav-item nav-link h3" href="/dashapp">Visualization Dashboard</a>
                 </div>
             </div>
-            <div class="d-flex justify-content-end">
-            <a class="navbar-brand" href="#">
-            <img src="/static/DAC_logo.png" width="30" height="30" class="d-inline-block align-top" alt="Virginia Tech">
-                
-            </a>
+            <div class="d-flex ml-auto">
+                <span class="navbar-brand" href="#">
+                    <img src="/static/assets/DAC_logo.png"
+                         width="30" height="30"
+                         class="d-inline-block"
+                         alt="Virginia Tech">
+                </span>
+            </div>
         </div>
-        </div>
-      
-        </nav>
-    
+    </nav>
+  
         <div class=" mr-3  container-fluid ">
         {%app_entry%}
         </div>
