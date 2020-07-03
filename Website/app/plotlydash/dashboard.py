fa39e5f1ce632f3bf3ab2147a2fcf46c7eb80b5e
ddatta <ddatta@vt.edu>
2020-07-02 22:21:01 -0400

    Major visualization changes

--- Website/app/plotlydash/dashboard.py
+++ Website/app/plotlydash/dashboard.py
@@ -10,7 +10,7 @@ pandarallel.initialize()
 sys.path.append('./../../..')
 sys.path.append('./../..')
 sys.path.append('./..')
-from cyberDataGenSite.data.common_utils import utils
+from cyberDataGenSite.data.common_utils import utils as common_utils
 import dash
 import dash_table
 import dash_bootstrap_components as dbc
@@ -45,28 +45,57 @@ def create_dashboard(server):
     )
 
     # Load DataFrame
-    data = fetch_network_data_v1()
+    data = read_data(_size=10000)
     # Custom HTML layout
     dash_app.index_string = html_layout
 
     # ========================================
-    # Create a Time series plot
+    # Create  Time series plots
     # ========================================
 
-    tab_obj = get_viz_tab_1(data)
-    time_series_Packets = get_TS_fig(figure_id=1, df=data, y_value='Packets')
-    time_series_Bytes = get_TS_fig(figure_id=2, df=data, y_value='Bytes')
+    tab_list = []
+    time_series_tab_dict = {
+        'background': 'Background Data',
+        'sshscan' : 'SSH Scan Netflows',
+        'spam'  : 'Spam Attack Netflows '
+    }
+    fig_idx = 1
+
+    for _type,_label  in time_series_tab_dict.items():
+        data = read_data(_type,_size=5000)
+        time_series_count = get_TS_fig_v2(figure_id=fig_idx, df=data, group_by='Protocol')
+        fig_idx += 1
+        # time_series_Bytes = get_TS_fig(figure_id=fig_idx, df=data, y_value='Bytes')
+        # fig_idx += 1
+        tab_element = dcc.Tab(
+            label= _label,
+            children=[
+                time_series_count
+            ]
+        )
+        tab_list.append(tab_element)
+
+
+    tab_container = dcc.Tabs(tab_list)
+    TimeSeries_tab_container = html.Div(
+        tab_container,
+        id="ts_tabs",
+        className="mx-auto"
+    )
+
     # ========================================
     # Create a visualization tab
     # ========================================
-
+    data = read_data(_size=10000)
+    tab_obj = get_viz_tab_1(data)
 
     dash_app.layout = html.Div(
         children=[
-            time_series_Packets,
-            time_series_Bytes,
+            TimeSeries_tab_container,
             tab_obj,
-            create_data_table(data)
+            create_data_table(_type='background'),
+            create_data_table(_type='sshscan'),
+            create_data_table(_type='spam')
         ],
         id='dash-container',
         className="container-fluid mr-4"
@@ -74,8 +103,55 @@ def create_dashboard(server):
 
     return dash_app.server
 
+def read_data(
+        _type = 'background',
+        _size = 100000
+):
+    DATA_LOC = './../data/Processed'
+    if type is None:
+        pass
+    import glob
 
-def create_data_table(df):
+    files = list(glob.glob( os.path.join( DATA_LOC, _type + '**','**.csv') )) + glob.glob( os.path.join( DATA_LOC, _type + '**.csv'))
+    df = None
+
+    for file in sorted(files):
+        _df = pd.read_csv(file, index_col=None)
+        if df is None:
+            df =_df
+        else:
+            df = df.append(_df, ignore_index=True)
+
+    df = df.rename(columns={
+        'sp': 'Source Port',
+        'dp': 'Destination Port',
+        'pr': 'Protocol',
+        'byt': 'Bytes',
+        'pkt': 'Packets',
+        'td': 'Time Duration',
+        'TS': 'TimeStamp',
+        'sa': 'Source Address',
+        'da': 'Destination Address',
+        'label': 'Label'
+    })
+    columns = [
+        'TimeStamp',
+        'Source Address',
+        'Source Port',
+        'Destination Port',
+        'Destination Address',
+        'Protocol',
+        'Bytes',
+        'Packets',
+        'Time Duration',
+        'Label'
+    ]
+    df = df[columns]
+    if _size < len(df):
+        df = df.sample( _size )
+    return df
+
+def create_data_table(_type):
     """
     Create Dash datatable from Pandas DataFrame.
     """
@@ -91,9 +167,15 @@ def create_data_table(df):
         'Time Duration',
         'Label'
     ]
+    # -----------------
+    # Read in data
+    # -----------------
+
+    df = read_data(_type)
     df = df[columns]
+
     table = dash_table.DataTable(
-        id='database-table',
+        id='database-table_'+_type,
         columns=[{"name": i, "id": i} for i in df.columns],
         data=df.to_dict('records'),
         sort_action="native",
@@ -119,15 +201,34 @@ def create_data_table(df):
                 },
                 'backgroundColor': 'green',
                 'color': 'white'
+            },
+            {
+                'if': {
+                      'filter_query': '{Label} = anomaly-sshscan',
+                      'column_id': 'Label'
+                },
+                'backgroundColor': 'red',
+                'color': 'white'
+            },
+            {
+                'if': {
+                    'filter_query': '{Label} = anomaly-spam',
+                    'column_id': 'Label'
+                },
+                'backgroundColor': 'red',
+                'color': 'white'
             }
+
         ]
     )
     table = html.Div(
         table,
         className='border border-100'
     )
+    type_str = get_display_string(_type)
+    header_label = ' Data Table :: ' + type_str
     header = html.Div(
-        html.P('Data Table'),
+        html.P(header_label),
         className="text-center h3"
     )
     table = html.Div(
@@ -183,9 +284,11 @@ def get_cat_plot(df, columns):
     df_1 = df_1.groupby(columns).size().reset_index(name='count')
 
     fig = px.parallel_categories(
-        df_1, dimensions=columns,
+        df_1,
+        dimensions=columns,
         color="count",
-        color_continuous_scale=px.colors.sequential.Pinkyl
+        color_continuous_scale=px.colors.sequential.Pinkyl,
+
     )
     fig.update_layout(
         autosize=False,
@@ -219,7 +322,8 @@ def get_violin_plot_div(df, y_column='Time Duration', x_col='Protocol'):
 # ===============================
 # Time Series plot
 # ===============================
-def get_TS_fig(figure_id, df, x_value='TS', y_value='Packets', group_by='Protocol'):
+
+def get_TS_fig(figure_id, df, x_value='TimeStamp', y_value='Packets', group_by='Protocol'):
     fig = plotly_utils.fetch_figure(figure_id)
     if fig is None:
         print('Figure not saved', file=sys.stdout)
@@ -255,6 +359,45 @@ def get_TS_fig(figure_id, df, x_value='TS', y_value='Packets', group_by='Protoco
     return time_series_div
 
 
+def get_TS_fig_v2(figure_id, df, x_value='TimeStamp',  group_by='Protocol'):
+    fig = plotly_utils.fetch_figure(figure_id)
+    if fig is None:
+        print('Figure not saved', file=sys.stdout)
+        df = df.copy()
+
+        # Modify as count
+        df = df.groupby(['TimeStamp',group_by]).size().reset_index(name="count")
+        y_value = "count"
+        fig = px.line(df, x=x_value, y=y_value, color=group_by)
+        fig.update_xaxes(
+            rangeslider_visible=True,
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=3, label="3h", step="hour", stepmode="backward"),
+                    dict(count=6, label="6h", step="hour", stepmode="backward"),
+                    dict(count=12, label="12h", step="hour", stepmode="backward"),
+                    dict(count=1, label="1d", step="day", stepmode="backward"),
+                    dict(count=1, label="TD", step="day", stepmode="todate"),
+                    dict(step="all")
+                ])
+            )
+        )
+        fig.update_layout(
+            font=dict(
+                family="Courier New",
+                size=12,
+                color="#7f7f7f"
+            ),
+            title_text=y_value)
+        plotly_utils.save_figure(fig, figure_name=figure_id)
+
+    time_series_viz = dcc.Graph(figure=fig)
+    time_series_div = html.Div(
+        time_series_viz,
+        className='container-fluid justify-content-center'
+    )
+    return time_series_div
+
 # =======================
 # An utility function
 # =======================
@@ -264,7 +407,7 @@ def modify_ports(df, port_columns):
     def aux(val):
         val = int(val)
         if val <= 1023:
-            return val
+            return 'System Port'
         if val <= 49151:
             return "Reg. Port"
         else:
@@ -281,21 +424,14 @@ def modify_ports(df, port_columns):
 # The design is hardcoded
 # =======================================
 def get_viz_tab_1(data):
+
     df = data.copy()
     # List to store each Tab specific data in the  Tabs container
     tab_list = []
     # ===================================
     # 1. Source Port to Destination Port
     # ===================================
-    # Group ports
-    df_port_modified = modify_ports(df, port_columns=['Source Port', 'Destination Port'])
-    columns = ['Source Port', 'Destination Port']
-    fig = get_cat_plot(df_port_modified, columns)
-    tab_1 = dcc.Tab(
-        label='Port Communication',
-        children=[html.Div(dcc.Graph(figure=fig), className='d-flex justify-content-center')]
-    )
-    tab_list.append(tab_1)
+
     # =====
     # 2. Source Port to Destination Port
     # =====
@@ -304,11 +440,10 @@ def get_viz_tab_1(data):
         label='Protocol Distribution',
         children=[html.Div(dcc.Graph(figure=fig), className='d-flex justify-content-center')]
     )
-    tab_list.append(tab_2)
 
     tab_3 = dcc.Tab(
         label='Time duration of connections',
-        children=[get_violin_plot_div(data)]
+        children=[get_violin_plot_div(data,y_column='Time Duration')]
     )
     tab_4 = dcc.Tab(
         label='# of Packets',
@@ -318,9 +453,21 @@ def get_viz_tab_1(data):
         label='# of Bytes',
         children=[get_violin_plot_div(data, y_column='Bytes')]
     )
+    # Group ports
+    df_port_modified = modify_ports(df, port_columns=['Source Port', 'Destination Port'])
+    columns = ['Source Port', 'Destination Port']
+    fig = get_cat_plot(df_port_modified, columns)
+    tab_1 = dcc.Tab(
+        label='Port Communication',
+        children=[html.Div(dcc.Graph(figure=fig), className='d-flex justify-content-center')]
+    )
+
     tab_list.append(tab_3)
     tab_list.append(tab_4)
     tab_list.append(tab_5)
+    tab_list.append(tab_2)
+    tab_list.append(tab_1)
+
     tab_container = dcc.Tabs(tab_list)
     tab_container = html.Div(
         tab_container,
@@ -328,3 +475,12 @@ def get_viz_tab_1(data):
         className="mx-auto"
     )
     return tab_container
+
+
+def get_display_string(_type):
+    if _type=='background':
+        return 'Background Traffic'
+    if _type=='sshscan':
+        return 'SSH Scan Traffic'
+    if _type=='spam':
+        return 'Spam Attack Traffic'
